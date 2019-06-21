56701be62490c7ed8498fd2f6605a896eb834c50
benjamincarlin <42980312+benjamincarlin@users.noreply.github.com>
2019-06-21 11:18:50 -0400

    Improved creating datasets script to auto generate relationships and … (#11)
    * Improved creating datasets script to auto generate relationships and update assets
    
    * removed additional copy of requirements.txt
    
    * first version using list comprehension, added unit test for invalid relationships
    
    * changed String to string in script, changed link to string in test files
    
    * changed if statementt logic, updated validation and unit test for column datatype, changed relationships to link in columns
    
    * added TODO for validaiton
    
    * updated if logic for creating relationships to be more readable

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -1,69 +1,160 @@
 #!/usr/bin/env python2
-
 import os
 import argparse
 import json
 
 
-def make_new_dataset(dataset_name, dataset_description, parent_directory, output_name):
+def make_new_dataset(dataset_name, dataset_description, input_tables_directory, input_assets_directory, output_name):
     """
-    Gets JSON file(s) for assets, tables and relationships, combines them and then write the combined JSON to disk.
+    Adds JSON files for assets and tables to a dataset, generates relationships and then writes the final JSON to disk.
     :param dataset_name: the schema name for the dataset
     :param dataset_description: description of the schema
-    :param parent_directory: the directory containing assets, relationships and tables JSON files
+    :param input_tables_directory: the directory containing tables JSON files
+    :param input_assets_directory: the directory containing assets JSON files
     :param output_name: the name of the output JSON, containing the combined assets, relationships and tables JSONs
     """
-    dataset = init_dataset(dataset_name=dataset_name,
-                           dataset_description=dataset_description)
-    dataset_with_tables = add_to_schema(dataset,
-                                        schema_type="tables",
-                                        parent_directory=parent_directory,
-                                        sub_directory="tables")
-    dataset_with_relationships = add_to_schema(dataset_with_tables,
-                                               schema_type="relationships",
-                                               parent_directory=parent_directory,
-                                               sub_directory="relationships")
-    dataset_with_assets = add_to_schema(dataset_with_relationships,
-                                        schema_type="assets",
-                                        parent_directory=parent_directory,
-                                        sub_directory="assets")
-    save_dataset_as_json(dataset_with_assets,
-                         output_name)
+    input_tables = get_json(parent_directory=input_tables_directory)
+    tables, relationships = create_tables_and_relationships(input_tables=input_tables)
 
+    input_assets = get_json(parent_directory=input_assets_directory)
+    assets = [
+        {
+            "name": input_assets[input_asset_name]["name"],
+            "rootTable": input_assets[input_asset_name]["rootTable"],
+            "rootColumn": input_assets[input_asset_name]["rootColumn"],
+            "tables": [{"name": table["name"], "columns": []} for table in tables],
+            "follow": [relationship["name"] for relationship in relationships]
+        } for input_asset_name in input_assets
+    ]
 
-def init_dataset(dataset_name, dataset_description):
-    """
-    Creates any empty dataset as a dict that has a study name, description, and schema.
-    :param dataset_name: the schema name for the dataset
-    :param dataset_description: description of the schema
-    """
     dataset = {
         "name": dataset_name,
         "description": dataset_description,
-        "schema": {
-            "tables": [],
-            "relationships": [],
-            "assets": []
-        }
+        "schema": {"tables": tables, "relationships": relationships, "assets": assets}
     }
-    return dataset
+    save_dataset_as_json(dataset=dataset, output_name=output_name)
 
 
-def add_to_schema(dataset, schema_type, parent_directory,  sub_directory):
+def get_json(parent_directory):
     """
-    Adds JSON files to the current dataset as a dict.
-    :param dataset: the combined dataset to be written to disk
-    :param schema_type: which part of the data set is being added to the schema, (either assets, relationships or tables)
-    :param parent_directory: the directory containing assets, relationships and tables JSON files
-    :param sub_directory: the child directory of either assets or tables JSON files
+    Adds JSON files to a dict with each key being the name field of a json file.
+    and the value being the corresponding json.
+    :param parent_directory: the directory containing JSON files to add to the dict
     """
-    json_files_path = os.path.join(parent_directory, sub_directory)
-    for file in os.listdir(json_files_path):
+    input_dict = {}
+    for file in os.listdir(parent_directory):
         if file.endswith(".json"):
-            with open(os.path.join(json_files_path, file), "r") as json_file:
+            with open(os.path.join(parent_directory, file), "r") as json_file:
                 json_as_dict = json.load(json_file)
-            dataset["schema"][schema_type].append(json_as_dict)
-    return dataset
+            # add the name field field of the json as a key to make each json searchable
+            input_dict[json_as_dict["name"]] = json_as_dict
+    return input_dict
+
+
+def create_tables_and_relationships(input_tables):
+    """
+    Creates tables and relationships from the input tables
+    :param input_tables: the input json tables that have been added to a dict
+    """
+    tables, relationships = [], []
+    for input_table_name in input_tables:
+        table, new_relationships = create_table_and_relationships(input_table=input_tables[input_table_name],
+                                                                  input_tables=input_tables)
+        tables.append(table)
+        relationships.extend(new_relationships)
+
+    return tables, relationships
+
+
+def create_table_and_relationships(input_table, input_tables):
+    """
+    Creates a table and relationships using the input tables fields and reference field.
+    :param input_table: the input table to be used to create a new table and relationships
+    :param input_tables: the input tables that have been added to a dict and used to validate the created relationships
+    """
+
+    input_table_name = input_table["name"]
+    columns, table_relationships = [], []
+    for input_column in input_table["columns"]:
+        column, new_relationships = create_column_and_relationships(input_relationships=input_column.get("links"),
+                                                                    column_name=input_column["name"],
+                                                                    column_datatype=input_column["datatype"],
+                                                                    column_array_of=input_column.get("array_of"),
+                                                                    input_table_name=input_table_name,
+                                                                    input_tables=input_tables)
+        columns.append(column)
+        table_relationships.extend(new_relationships)
+
+    table = {"name": input_table_name, "columns": columns}
+
+    return table, table_relationships
+
+
+def create_column_and_relationships(input_relationships,
+                                    column_name,
+                                    column_datatype,
+                                    column_array_of,
+                                    input_table_name,
+                                    input_tables):
+    """
+    Creates a column and relationships, keeping ony the name, data type and column field from the input column.
+    :param input_relationships: the list of relationships from an input JSON column
+    :param column_name: the name of the input JSON column
+    :param column_datatype: the primitive type enforced for values of the input JSON column
+    :param column_array_of: indicates if the column values should be in array
+    :param input_table_name: the name of input JSON table that has the input_column
+    :param input_tables: the input tables that have been added to a dict and used to validate the created relationships
+    """
+    relationships = []
+    if input_relationships:
+        if not any([validate_table_and_column(table_name=input_relationship["table_name"],
+                                              column_name=input_relationship["column_name"],
+                                              column_datatype=column_datatype,
+                                              input_tables=input_tables)
+                    for input_relationship in input_relationships]):
+            raise SystemExit("Error: The relationship(s) provided for table, {}, column {} are not valid"
+                             .format(input_table_name, column_name))
+
+        relationships = [
+            {
+                "name": "{}_{}_to_{}_{}".format(input_table_name,
+                                                column_name,
+                                                input_relationship["table_name"],
+                                                input_relationship["column_name"]),
+                "from": {"table": input_table_name, "column": column_name, "cardinality": "one"},
+                "to": {
+                        "table": input_relationship["table_name"],
+                        "column": input_relationship["column_name"],
+                        "cardinality": "many" if column_array_of else "one"
+                      }
+            }
+            for input_relationship in input_relationships]
+
+    column = {
+        "name": column_name,
+        "datatype": column_datatype,
+        "array_of": column_array_of is True
+    }
+
+    return column, relationships
+
+
+# TODO add validation for duplicate assets, tables, columns and relationships
+def validate_table_and_column(table_name, column_name, column_datatype, input_tables):
+    """
+    Validates a relationship checking the the given table and column exist in input tables and the datatype is correct.
+    :param table_name: the name of table containing has the input_column that will be validated against the input tables
+    :param column_name: the name of the input_column that will be validated against the input tables
+    :param column_datatype: the primitive type enforced for values of the input JSON column
+    :param input_tables: the input tables that have been added to a dict and used to validate the created relationships
+    """
+    for input_table_name in input_tables:
+        if input_table_name == table_name:
+            for input_column in input_tables[input_table_name]["columns"]:
+                if input_column["name"] == column_name:
+                    return input_column["datatype"] == column_datatype
+
+    return False
 
 
 def save_dataset_as_json(dataset, output_name):
@@ -73,10 +164,7 @@ def save_dataset_as_json(dataset, output_name):
     :param output_name: the name of the output JSON, containing the combined assets, relationships and tables JSONs
     """
     with open(output_name, "w") as output_file:
-        json.dump(dataset,
-                  output_file,
-                  sort_keys=False,
-                  indent=2)
+        json.dump(dataset, output_file, sort_keys=False, indent=2)
 
 
 if __name__ == "__main__":
@@ -92,11 +180,16 @@ if __name__ == "__main__":
                         dest="dataset_description",
                         required=True,
                         help="description of the schema")
-    parser.add_argument("--input-directory",
+    parser.add_argument("--input-tables-directory",
                         "-i",
-                        dest="input_directory",
+                        dest="input_tables_directory",
                         required=True,
-                        help="the directory containing assets, relationships and tables JSON files")
+                        help="the directory containing tables JSON files")
+    parser.add_argument("--input-assets-directory",
+                        "-i",
+                        dest="input_assets_directory",
+                        required=True,
+                        help="the directory containing assets JSON file(s)")
     parser.add_argument("--output-name",
                         "-o",
                         dest="output_name",
@@ -106,5 +199,6 @@ if __name__ == "__main__":
 
     make_new_dataset(dataset_name=args.dataset_name,
                      dataset_description=args.dataset_description,
-                     parent_directory=args.input_directory,
+                     input_tables_directory=args.input_directory,
+                     input_assets_directory=args.input_assets_directory,
                      output_name=args.output_name)
--- test/unit_test_create_dataset.py
+++ test/unit_test_create_dataset.py
@@ -3,13 +3,14 @@
 import os
 import unittest
 import tempfile
-from scripts.create_dataset import make_new_dataset
+from scripts import create_dataset
 import json
 from deepdiff import DeepDiff
+import errno
 
 
 class CreateDatasetTest(unittest.TestCase):
-    def test_files(self):
+    def test_make_new_dataset(self):
         # file paths for truth files
         current_file_dir = os.path.dirname(__file__)
         truth_output_schema_json = os.path.join(current_file_dir, "input_files/test_input_dataset.json")
@@ -18,10 +19,11 @@ class CreateDatasetTest(unittest.TestCase):
         output_dataset = os.path.join(tempfile.mkdtemp(), 'test_output_dataset')
 
         # run the create dataset python script with given input JSON files
-        make_new_dataset(dataset_name="dataset_name",
-                         dataset_description="dataset_description",
-                         parent_directory=os.path.join(current_file_dir, "input_files"),
-                         output_name=output_dataset)
+        create_dataset.make_new_dataset(dataset_name="dataset_name",
+                                        dataset_description="dataset_description",
+                                        input_tables_directory=os.path.join(current_file_dir, "input_files/tables"),
+                                        input_assets_directory=os.path.join(current_file_dir, "input_files/assets"),
+                                        output_name=output_dataset)
 
         # compare expected outputs from python script to truth files
         with open(truth_output_schema_json, "r") as truth_file:
@@ -30,9 +32,49 @@ class CreateDatasetTest(unittest.TestCase):
                 truth_dict = json.load(truth_file)
         ddiff = DeepDiff(output_dict, truth_dict, ignore_order=True)
 
+        # if there are differences
         self.assertTrue(len(ddiff) == 0,
-                        "Error: The created json is not equal to truth file")
+                        "Error: The created json is not equal to truth file\n{}".format(ddiff))
 
+    def test_validate_table_and_column(self):
+        current_file_dir = os.path.dirname(__file__)
+        input_tables = create_dataset.get_json(os.path.join(current_file_dir, "input_files/tables"))
+
+        # check an expected valid table and column pair
+        self.assertTrue(create_dataset.validate_table_and_column(table_name="table_2",
+                                                                 column_name="column_1",
+                                                                 column_datatype="string",
+                                                                 input_tables=input_tables),
+                        "Error: The valid table and column could not be validated using input JSON tables")
+
+        # check an expected invalid input column datatype
+        self.assertFalse(create_dataset.validate_table_and_column(table_name="table_2",
+                                                                  column_name="column_1",
+                                                                  column_datatype="float",
+                                                                  input_tables=input_tables),
+                        "Error: The invalid input column datatype was validated using input JSON tables")
+
+        # check an expected invalid table name
+        self.assertFalse(create_dataset.validate_table_and_column(table_name="invalid_table_name",
+                                                                  column_name="column_1",
+                                                                  column_datatype="string",
+                                                                  input_tables=input_tables),
+                         "Error: The invalid table name was validated using input JSON tables")
+
+        # check an expected invalid column name
+        self.assertFalse(create_dataset.validate_table_and_column(table_name="table_4",
+                                                                  column_name="invalid_column_name",
+                                                                  column_datatype="string",
+                                                                  input_tables=input_tables),
+                         "Error: The invalid column name was validated using input JSON tables")
+
+        invalid_input_tables = create_dataset.get_json(os.path.join(current_file_dir, "input_files/invalid_tables"))
+        # check a invalid relationship with two columns containing two different datatype
+        self.assertFalse(create_dataset.validate_table_and_column(table_name="table_2",
+                                                                  column_name="column_1",
+                                                                  column_datatype="float",
+                                                                  input_tables=invalid_input_tables),
+                         "Error: The invalid relationship with 2 columns containing 2 different datatype was validated")
 
 
 if __name__ == '__main__':
