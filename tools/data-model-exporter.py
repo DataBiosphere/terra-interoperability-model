bceaa36669eec405a02a1a8ba7d2bbbcb60d7c21
Quazi Hoque <qhoque@broadinstitute.org>
2021-02-18 10:43:45 -0500

    new skeleton script

---
+++ tools/data-model-exporter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Create a basic CLI tool that parses the arg list defined in the spec
+# (as linked in the parent epic) and hands off the args to a stub driver method
+
+# Dependency management via poetry
+
+import argparse
+import poetry
+
+
+# arguments defined in spec
+# 1) a class listing file
+# 2) a path to the data model *.TTL file we are transforming
+
+
+
+
+
+
+
+# output - receive a properly transformed JSON schema file.
